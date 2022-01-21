3ed0734024ddbd605d58af4a921a86e7129145c2
Thomas Geske <thomasge@microsoft.com>
2022-01-21 17:04:56 +0100

    added  OverwriteBusinessLogic

---
+++ src/Sample.iPaaS.OData/Model/OverwriteBusinessLogic.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.iPaaS.OData.Model
+{
+    public class OverwriteBusinessLogic
+    {
+        public int OverwriteBusinessLogic_Id { get; set; }
+        public int Store_Id { get; set; }
+        public string Action { get; set; }
+        public DateTime Date { get; set; }
+        public string OpenHours { get; set; }
+        public string Comment { get; set; }
+    }
+}
