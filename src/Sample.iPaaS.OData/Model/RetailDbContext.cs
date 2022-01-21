e71075ef1fde97c6aef2d6371d0a6cff4e9d750a
Thomas Geske <thomasge@microsoft.com>
2022-01-21 15:28:22 +0100

    added new OData project

---
+++ src/Sample.iPaaS.OData/Model/RetailDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sample.iPaaS.OData.Model
+{
+    public class RetailDbContext : DbContext
+    {
+        public RetailDbContext(DbContextOptions<RetailDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<StoreCalendar> StoreCalendarItems { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StoreCalendar>().ToView("StoreCalendar", "BankHoliday_V100").HasNoKey();
+        }
+    }
+}
