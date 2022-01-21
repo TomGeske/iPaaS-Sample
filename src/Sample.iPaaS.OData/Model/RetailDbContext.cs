8ab3452d5f31e0d59f1605845fbdf321ebcc3262
Thomas Geske <thomasge@microsoft.com>
2022-01-21 16:49:57 +0100

    added Product and DiscountedProduct

--- src/Sample.iPaaS.OData/Model/RetailDbContext.cs
+++ src/Sample.iPaaS.OData/Model/RetailDbContext.cs
@@ -11,11 +11,15 @@ namespace Sample.iPaaS.OData.Model
 
         public DbSet<StoreCalendar> StoreCalendarItems { get; set; } = null!;
         public DbSet<Store> Stores { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<DiscountedProduct> DiscountedProducts { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<StoreCalendar>().ToView("StoreCalendar", "BankHoliday_V100").HasNoKey();
             modelBuilder.Entity<Store>().ToView("Store", "BankHoliday_V100").HasNoKey();
+            modelBuilder.Entity<Product>().ToView("Product", "StockListMDM_V100").HasNoKey();
+            modelBuilder.Entity<DiscountedProduct>().ToView("DiscountedProduct", "StockListSAP_V100").HasNoKey();
         }
     }
 }
