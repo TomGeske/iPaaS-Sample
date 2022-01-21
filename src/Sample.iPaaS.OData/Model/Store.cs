568d15c4fd15d95b0a8d629782bb3313ab4f14aa
Thomas Geske <thomasge@microsoft.com>
2022-01-21 16:49:42 +0100

    init nullable fields

--- src/Sample.iPaaS.OData/Model/Store.cs
+++ src/Sample.iPaaS.OData/Model/Store.cs
@@ -5,12 +5,12 @@ namespace Sample.iPaaS.OData.Model
     public class Store
     {
         public int StoreId { get; set; }
-        public string StoreName { get; set; }
-        public string Canton { get; set; }
-        public string Streeet { get; set; }
+        public string StoreName { get; set; } = String.Empty;
+        public string Canton { get; set; } = String.Empty;
+        public string Streeet { get; set; } = String.Empty;
         public Int16 PLZ { get; set; }
-        public string City { get; set; }
-        public string StoreType { get; set; }
+        public string City { get; set; } = String.Empty;
+        public string StoreType { get; set; } = String.Empty;
         public int WeekKalendarId { get; set; }
     }
 }
