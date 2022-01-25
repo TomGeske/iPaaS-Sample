ab8efda9ee1bb4d1aed40768dd3a15cf10efd5bd
Thomas Geske <thomasge@microsoft.com>
2022-01-25 10:52:12 +0100

    added application insights

--- src/Sample.iPaaS.OData/Program.cs
+++ src/Sample.iPaaS.OData/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<RetailDbContext>(options => options.UseSqlServer(b
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
